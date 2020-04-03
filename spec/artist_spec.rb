60c8fa14886bfff0c2b1fd71c4b34936e29930d6
Mariam Haider <mfh.haider@gmail.com>
2020-04-03 20:43:08 +0000

    Automatically backed up by Learn

--- spec/artist_spec.rb
+++ spec/artist_spec.rb
@@ -31,7 +31,7 @@ describe "Artist" do
       it "takes in an argument of a song and associates that song with the artist by telling the song that it belongs to that artist" do
         hello = Song.new("Hello")
         adele.add_song(hello)
-        binding.pry
+        #binding.pry
         expect(adele.songs).to include(hello)
         expect(hello.artist).to eq(adele)
       end
